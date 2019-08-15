1422d462e31678ae5bf3812bba4f2472897f96b5
Guy Lin <xup6ru03xu4@gmail.com>
2019-08-16 06:43:45 +0800

    Empty_return_0

---
+++ CountingDuplicates/CountingDuplicatesKata.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CountingDuplicates
+{
+    public class CountingDuplicatesKata
+    {
+        public int DuplicateCount(string str)
+        {
+            if (str.Trim() != "")
+            {
+            }
+            return 0;
+        }
+    }
+}
---
+++ CountingDuplicates/CountingDuplicatesKataTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CountingDuplicates
+{
+    [TestClass]
+    public class CountingDuplicatesKataTest
+    {
+        private readonly CountingDuplicatesKata _kata = new CountingDuplicatesKata();
+
+        [TestMethod]
+        public void Empty_return_0()
+        {
+            var actual = _kata.DuplicateCount("");
+
+            Assert.AreEqual(0, actual);
+        }
+    }
+}
