87ba3e42905344ceff9a4f1f7993030b9f88281f
Guy Lin <xup6ru03xu4@gmail.com>
2019-08-17 14:42:14 +0800

    refactor

--- CountingDuplicates/CountingDuplicatesKata.cs
+++ CountingDuplicates/CountingDuplicatesKata.cs
@@ -19,20 +19,33 @@ namespace CountingDuplicates
 
         private static int Count(string strBuffer)
         {
-            if (strBuffer != "")
+            if (IsEmptyString(strBuffer)) return 0;
+
+            if (IsExitsAboveTwice(strBuffer))
             {
-                if (strBuffer.ToLower().ToCharArray().Count(x => x == strBuffer.ToLower()[0]) >= 2)
-                {
-                    strBuffer = strBuffer.ToLower().Replace(strBuffer[0].ToString().ToLower(), "");
-                    return 1 + Count(strBuffer);
-                }
-                else
-                {
-                    strBuffer = strBuffer.ToLower().Replace(strBuffer[0].ToString().ToLower(), "");
-                    return Count(strBuffer);
-                }
+                strBuffer = RemoveAllFirstWord(strBuffer);
+                return 1 + Count(strBuffer);
             }
-            return 0;
+            else
+            {
+                strBuffer = RemoveAllFirstWord(strBuffer);
+                return Count(strBuffer);
+            }
+        }
+
+        private static string RemoveAllFirstWord(string strBuffer)
+        {
+            return strBuffer.ToLower().Replace(strBuffer[0].ToString().ToLower(), "");
+        }
+
+        private static bool IsExitsAboveTwice(string strBuffer)
+        {
+            return strBuffer.ToLower().ToCharArray().Count(x => x == strBuffer.ToLower()[0]) >= 2;
+        }
+
+        private static bool IsEmptyString(string strBuffer)
+        {
+            return strBuffer == "";
         }
     }
 }
